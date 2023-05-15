c662a1c7f9af2e7792084b9d61526601748fb8ad
Tom Heaton <thomas.m.heaton@gmail.com>
2023-05-15 14:29:19 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="font-extrabold text-4xl tracking-tighter">flashcards</h1>
+    </div>
+  );
+}
